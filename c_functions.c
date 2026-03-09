abf64f664a5c8af0c077e93e86856bcc1babd95c
vez767 <wa3960f@greenwich.ac.uk>
2026-03-09 08:54:01 +0000

    feat: implement integer addition module and header guards

--- c_functions.c
+++ c_functions.c
@@ -1,1 +1,28 @@
+#include <stdint.h>
 #include "header_file.h"
+
+int add_int(uint16_t term_1,uint16_t term_2)
+{
+    uint16_t sum = term_1 + term_2;
+    return sum;
+}
+
+int subtract_int(uint16_t term_1,uint16_t term_2)
+{
+    uint16_t res = term_1 - term_2;
+    return res;
+}
+
+int multiply_int(uint16_t factor_1,uint16_t factor_2)
+{
+    uint32_t product = factor_1 * factor_2;
+    return product;
+}
+
+int divide_int(uint16_t divisor, uint16_t dividend)
+{
+    uint32_t product = divisor / dividend;
+    return product;
+}
+
+
--- header_file.h
+++ header_file.h
@@ -1,4 +1,10 @@
 #ifndef HEADER_FILE_H
 #define HEADER_FILE_H
+#include <stdint.h>
+int add_int(uint16_t term_1,uint16_t term_2);
+int subtract_int(uint16_t term_1,uint16_t term_2);
+int multiply_int(uint16_t factor_1,uint16_t factor_2);
+int divide_int(uint16_t divisor, uint16_t dividend);
+
 
 #endif 
